9995dcf301adfe75b17195e3b9bec91b24b673d5
briankim <brianhkim13@gmail.com>
2023-08-22 13:43:31 -0500

    beginnings

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+//make a todo list app
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Todo List</h1>
+        <input type="text" placeholder="Add a new todo"></input>
+        <ul>
+          <li>Get up</li>
+          <li>Brush teeth</li>
+          <li>Take a shower</li>
+          <li>Get dressed</li>
+          <li>Go to work</li>
+        </ul>
       </header>
     </div>
   );
