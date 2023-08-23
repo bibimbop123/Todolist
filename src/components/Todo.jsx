907897d7009f8e6ce538acd9804a5e174aade552
briankim <brianhkim13@gmail.com>
2023-08-23 14:30:19 -0500

    made edit delete and complete buttons for todolist

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,10 +1,46 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 export default function Todo() {
   const [list, setList] = useState([]);
   const [input, setInput] = useState("");
+  const [edit, setEdit] = useState(false);
+  const [editInput, setEditInput] = useState("");
+  const [completed, setCompleted] = useState([]);
+
+  const handleAdd = () => {
+    setList([...list, input]);
+    setInput("");
+  };
+
+  const handleDelete = (index) => {
+    const newList = [...list];
+    newList.splice(index, 1);
+    setList(newList);
+  };
+
+  const handleEdit = (item, index) => {
+    setEdit(!edit);
+    setEditInput(item);
+  };
+
+  const handleUpdate = (index) => {
+    const newList = [...list];
+    newList[index] = editInput;
+    setList(newList);
+    setEdit(false);
+  };
+
+  const handleComplete = (index) => {
+    const newCompleted = [...completed];
+    if (newCompleted.includes(index)) {
+      const indexToRemove = newCompleted.indexOf(index);
+      newCompleted.splice(indexToRemove, 1);
+    } else {
+      newCompleted.push(index);
+    }
+    setCompleted(newCompleted);
+  };
 
   return (
     <div>
@@ -13,19 +49,31 @@ export default function Todo() {
         value={input}
         onChange={(e) => setInput(e.target.value)}
       />
-      <button onClick={() => setList([...list, input])}>Add</button>
+      <button onClick={handleAdd}>Add</button>
       <ul>
         {list.map((item, index) => (
-          <div>
-            <li key={index}>{item}</li>
-            <button
-              onClick={() => {
-                const newList = [...list];
-                newList.splice(index, 1);
-                setList(newList);
+          <div key={index}>
+            <li
+              style={{
+                textDecoration: completed.includes(index) ? "line-through" : "",
               }}
             >
-              Delete
+              {item}
+            </li>
+            {edit && (
+              <>
+                <input
+                  type="text"
+                  value={editInput}
+                  onChange={(e) => setEditInput(e.target.value)}
+                />
+                <button onClick={() => handleUpdate(index)}>Update</button>
+              </>
+            )}
+            <button onClick={() => handleEdit(item, index)}>Edit</button>
+            <button onClick={() => handleDelete(index)}>Delete</button>
+            <button onClick={() => handleComplete(index)}>
+              {completed.includes(index) ? "Uncomplete" : "Complete"}
             </button>
           </div>
         ))}
